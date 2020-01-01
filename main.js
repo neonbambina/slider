bae541897c854c4caf409478839ef89130742f5c
Ariel George <arielgeorge@ariels-air.lan>
2019-12-31 19:08:36 -0600

    odqnvlandsc

--- main.js
+++ main.js
@@ -3,7 +3,23 @@ var app = new Vue({
     data: {
         product: 'Socks',
         image: 'images/vmSocks-green-onWhite.jpg',
-        inventory: 8
+        inventory: 8,
+        onSale: true,
+        details: ["80% cotton", "20% polyester", "gender-neutral"],
+
+        variants: 
+        {
+            variantId: 2234,
+            variantColor: "green"
+        },
+        {
+            variantId: 2235,
+            variantColor: "blue"
+        },
+       
+
+
+
 
     }
 })
